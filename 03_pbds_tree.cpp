1f8f9ebd67bec8e8c953da2e94c632fa6462ec58
nevikw39 <nevikw39@icloud.com>
2020-05-04 19:31:45 +0800

    pbds

---
+++ 03_pbds_tree.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+int main()
+{
+    __gnu_pbds::tree<string, int> m;
+    m["circ"] = 33;
+    m["ddc"] = 39;
+    cout << "circ = " << m["circ"] << '\n';
+    for (const pair<string, int> &i : m) // const auto &i : m
+        cout << "key: " << i.first << ", val: " << i.second << '\n';
+    if (m.find("non-exist key") != m.end())
+        cout << "The key is exist.\n";
+    else
+        cout << "The key is not exist.\n";
+    cout << "If you access by a non-exist key, then you get " << m["non-exist key"] << '\n';
+    if (m.find("non-exist key") != m.end())
+        cout << "The key is now exist.\n";
+    else
+        cout << "The key is still not exist.\n";
+    m["qwert"] = 87;
+    return 0;
+}
